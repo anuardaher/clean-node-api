0a3c29a57a8db4fd3197bd84899f869ddb8471b1
Anuar <anuar_daher@hotmail.com>
2020-12-13 21:31:23 -0300

    refactor: error class and http helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,18 @@
+import { httpRequest, httpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
-    return {
-      statusCode: 400
+  handle (httpRequest: httpRequest): httpResponse {
+    if (!httpRequest.body.name) {
+      return badRequest(new MissingParamError('name'))
+    } else if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'))
+    } else {
+      return {
+        statusCode: 200,
+        body: 'Ok'
+      }
     }
   }
 }
