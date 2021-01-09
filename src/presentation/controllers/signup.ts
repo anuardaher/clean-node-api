2932c8da35a0d8fd17f74d74599e64ec679afebc
Anuar <anuar_daher@hotmail.com>
2021-01-09 00:03:53 -0300

    feat: email test implemented

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,25 @@
 import { httpRequest, httpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
+export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
 
-export class SignUpController {
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: httpRequest): httpResponse {
-    const requiredFields = ['name', 'email', 'password']
-    let error: any
-    requiredFields.forEach((field) => {
-      if (!httpRequest.body[field]) {
-        error = badRequest(new MissingParamError(field))
-      }
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    const error = requiredFields.find((field) => {
+      return !httpRequest.body[field]
     })
-    return error || { statusCode: 200, body: 'Ok' }
+    if (error) return badRequest(new MissingParamError(error))
+    if (!this.emailValidator.isValid(httpRequest.body.email)) {
+      return badRequest(new InvalidParamError('email'))
+    }
+    return { statusCode: 200, body: 'Ok' }
   }
 }
